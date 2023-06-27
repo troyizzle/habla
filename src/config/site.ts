593d3e1291ef5860be176be3ad0515a87e755b44
Troy Underwood <troyizzle@gmail.com>
2023-06-27 14:13:24 -0500

    feat: og image

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig = {
   name: "Habla",
   description: "An open source twitter clone built with Next.js 13",
   url: "",
-  ogImage: "",
+  ogImage: "https://habla-gilt.vercel.app/opengraph-image.png",
   links: {
     github: "https://github.com/troyizzle/habla"
   }
